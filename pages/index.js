92dc5b1d85d168e16c73327875a9729f1aa5ea24
Junior Lourenço <juniorminaj101@gmail.com>
2021-02-09 13:10:38 -0400

    configurado as regras do eslint

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,13 @@ export const QuizContainer = styled.div`
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
-      <Head />
+      <Head>
+        <title>Junior Quiz</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Junior Quiz"/>
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Content>
