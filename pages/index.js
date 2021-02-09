7675a7852c097dece78b9d46973f235279196f41
Junior Lourenço <juniorminaj101@gmail.com>
2021-02-09 13:13:19 -0400

    correçao metatags

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://metatags.io/" />
         <meta property="og:title" content="Junior Quiz"/>
-        <meta property="og:image" content="https://www.pixelstalk.net/wp-content/uploads/images2/Free-Zelda-Wallpapers-HD.jpg"></meta>
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
       </Head>
       <QuizContainer>
         <Widget>
