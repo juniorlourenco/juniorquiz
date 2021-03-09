39a4b4f58058a7319c0348367bcbeac5d50cbaeb
Junior Lourenço <juniorminaj101@gmail.com>
2021-03-09 00:30:58 -0400

    cors liberado

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,5 +1,16 @@
 import db from '../../db.json';
 
 export default function (req, res) {
+    //libera cors
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
+    res.setHeader('Acess-Control-Allow-Credentials', true);
+    res.setHeader('Acess-Control-Allow-Origin', '*');
+    res.setHeader('Acess-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+    //libera cors
+    
     res.json(db);
 }
